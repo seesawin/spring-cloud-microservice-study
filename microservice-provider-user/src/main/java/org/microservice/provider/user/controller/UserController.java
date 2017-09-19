71eb0453a615a3f809bfa59291615dc0afa98cb5
seesawin <seesawin@gmail.com>
2017-09-19 17:49:49 +0800

    add service provider

---
+++ microservice-provider-user/src/main/java/org/microservice/provider/user/controller/UserController.java
@@ -0,0 +1,42 @@
+package org.microservice.provider.user.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 作用：
+ * ① 测试服务实例的相关内容
+ * ② 为后来的服务做提供
+ * @author eacdy
+ */
+@RestController
+public class UserController {
+  @Autowired
+  private DiscoveryClient discoveryClient;
+
+  /**
+   * 注：@GetMapping("/{id}")是spring 4.3的新注解等价于：
+   * @RequestMapping(value = "/id", method = RequestMethod.GET)
+   * 类似的注解还有@PostMapping等等
+   * @param id
+   * @return user信息
+   */
+  @GetMapping("/{id}")
+  public String findById(@PathVariable Long id) {
+    return "seesawin test id : " + id;
+  }
+
+  /**
+   * 本地服务实例的信息
+   * @return
+   */
+  @GetMapping("/instance-info")
+  public ServiceInstance showInfo() {
+    ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
+    return localServiceInstance;
+  }
+}
